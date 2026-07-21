0e0f58a04f18abed4220353c0d8fa87ebac4f847
Sarveshwar Hirve <sarveshwar@gmail.com>
2026-07-21 15:03:00 +0530

    Completed till Javascript

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-let post = document.getElementById("post");       // we are calling post id
-let postinput = document.getElementById("postinput");  // we are calling postinput id
-let newpost = document.getElementById("newpost") ;     // we are calling new post id
-let allposts = document.getElementById("allposts");
+let searchInput = document.getElementById("searchInput")
+let searchBtn1 = document.getElementById("searchBtn")
+let postinput = document.getElementById("postinput")
+let postBtn = document.getElementById("post")
+let allposts = document.getElementById("allposts")
 
-post.addEventListener("click",function(){
-    if (postinput.value == ""){
-        return;
+
+
+
+postBtn.addEventListener("click",function(){
+    let postText = postinput.value;                    // this will whatever text type by user { post.text will make that text in variable}
+    if(postText == ""){
+        return;                                       // this will not post any thing if input is emapt
     }
-    allposts.innerHTML += "<p>" + postinput.value + "</p>";     // add new post inside allpost
-    postinput.value = "" ;           // clear input box after posting
+    let newPost = document.createElement("p");
+    newPost.innerText = postText;                     // puts the user text inside the <p>
+    let deleteBtn = document.createElement("button")  // we have created new button "delete"
+    deleteBtn.innerText = "Delete"                    // the "delete" word is shown on deleteBtn button
+    deleteBtn.addEventListener("click",function(){
+        newPost.remove();                             // this will remove new post 
+        deleteBtn.remove();                          // this will remove delete button
+    });
+    allposts.appendChild(newPost)                     // add the <p> to the web page
+    allposts.appendChild(deleteBtn)                   // button will come below the new post
+    postinput.value = "";                             // clear the input after posting
+    console.log(postText);                             // this is display that text in console
 });
+
